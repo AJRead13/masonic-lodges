f8978e63829ca08b53125bccbb9bd2f762de6449
aheath <aaron.heath5447@gmail.com>
2024-05-17 21:58:02 -0400

    Initial backend setup. Preliminary models, resolvers, and typedefs setup. Server starts

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,11 @@
+const { User } = require('../models');
+
+const resolvers = {
+    Query: {
+        user: async (parent, { userId }) => {
+            return User.findOne({_id: userId});
+        }
+    }
+}
+
+module.exports = resolvers;
